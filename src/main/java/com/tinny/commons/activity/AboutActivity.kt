a21f778d8e39bdbc2493a72fef6fe339fc28f112
rajashakher.valluru <rajashakher.valluru@ril.com>
2019-06-06 19:18:11 +0530

    Test

--- src/main/java/com/tinny/commons/activity/AboutActivity.kt
+++ src/main/java/com/tinny/commons/activity/AboutActivity.kt
@@ -1,40 +1,39 @@
 package com.tinny.commons.activity
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.tinny.commons.*
-import com.tinny.commons.extentions.*
-import com.tinny.commons.helper.CommonConstants
-import com.tinny.commons.helper.CommonTextIcons
-import kotlinx.android.synthetic.main.about_row.view.*
+import com.tinny.commons.Intent_AppIcon
+import com.tinny.commons.Intent_AppName
+import com.tinny.commons.Intent_AppVesion
+import com.tinny.commons.R
+import com.tinny.commons.extentions.getStoreUrl
+import com.tinny.commons.extentions.launchViewIntent
+import com.tinny.commons.extentions.setFont
 import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.android.synthetic.main.row.view.*
 import java.util.*
 
 class AboutActivity : AppCompatActivity(),View.OnClickListener {
 
-    val emailId= "tinnymobileapps@gmail.com"
     override fun onClick(v: View?) {
         when(v!!.id){
-            R.id.moreApps ->{
-               launchViewIntent("https://play.google.com/store/apps/developer?id=Tinnymobileapps")
-//                launchViewIntent("https://play.google.com/store/apps/developer?id=Simple+Puzzle+games")
-
-            }
+            R.id.moreApps ->{ launchViewIntent("https://play.google.com/store/apps/dev?id=9070296388022589266") }
             R.id.rateUs ->{launchViewIntent("market://details?id=$packageName")}
-            R.id.reportBugs ->{reportBug()}
+            R.id.reportBugs ->{}
             R.id.share ->{onClickShare()}
         }
+
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        intent.getStringExtra(Intent_AppName)
-        txtAppName.text = intent.getStringExtra(Intent_AppName)
+        txtAppName.text = getString(R.string.app_name)
         imgApp.setImageResource(intent.getIntExtra(Intent_AppIcon,0))
 
         moreApps.txtTitle.text = getString(R.string.more_apps)
@@ -48,37 +47,24 @@ class AboutActivity : AppCompatActivity(),View.OnClickListener {
 
         reportBugs.txtTitle.text = getString(R.string.report_bug)
         reportBugs.txtDesc.text =getString(R.string.report_bug_desc)
-        setupCopyright()
 
-        if (intent.getBooleanExtra(Intent_Translation,true)){
-            rrTrans.makeVisible()
-        }else{
-            rrTrans.makeGone()
-        }
+        moreApps.txtIcon.setFont(this,getString(R.string.f_more_apps),"icomoon_common.ttf")
+        rateUs.txtIcon.setFont(this,getString(R.string.f_full_star),"icomoon_common.ttf")
+        share.txtIcon.setFont(this,getString(R.string.f_fav),"icomoon_common.ttf")
+        reportBugs.txtIcon.setFont(this,getString(R.string.f_bug),"icomoon_common.ttf")
 
-        moreApps.txtIcon.setFontWithColor(this, getString(R.string.f_more_apps), CommonConstants.icomoonCommon, resources.getColor(R.color.md_grey_black))
-        rateUs.txtIcon.setFontWithColor(this, getString(R.string.f_full_star), CommonConstants.icomoonCommon, resources.getColor(R.color.md_grey_black))
-        share.txtIcon.setFontWithColor(this, getString(R.string.f_fav), CommonConstants.icomoonCommon, resources.getColor(R.color.md_grey_black))
-        reportBugs.txtIcon.setFontWithColor(this, getString(R.string.f_bug), CommonConstants.icomoonCommon, resources.getColor(R.color.md_grey_black))
-        txtIconTran.setFontWithColor(this, CommonTextIcons.F_Translation, CommonConstants.icomoonCommon, resources.getColor(R.color.md_grey_black))
-
-
-
-        txtTranHelp.setOnClickListener {
-            val mIntent = Intent(Intent.ACTION_SENDTO)
-            mIntent.data = Uri.parse("mailto:")
-            mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emailId))
-            mIntent.putExtra(Intent.EXTRA_SUBJECT, intent.getStringExtra(Intent_AppName) + " (v-"+ intent.getStringExtra(Intent_AppVesion) + " -- "+ "Helping in text translation")
-            startActivity(Intent.createChooser(mIntent, "Send Email"))
-        }
-//        Toast.makeText(this,packageName,Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,packageName,Toast.LENGTH_SHORT).show()
         moreApps.setOnClickListener(this)
         rateUs.setOnClickListener(this)
         share.setOnClickListener(this)
         reportBugs.setOnClickListener(this)
+        setupCopyright()
     }
 
+
+
     private fun onClickShare() {
+
             val text = String.format(getString(R.string.share_text), intent.getStringExtra(Intent_AppName), getStoreUrl())
             Intent().apply {
                 action = Intent.ACTION_SEND
@@ -87,20 +73,14 @@ class AboutActivity : AppCompatActivity(),View.OnClickListener {
                 type = "text/plain"
                 startActivity(Intent.createChooser(this, getString(R.string.invite_via)))
             }
+
     }
 
     private fun setupCopyright() {
-        val versionName = intent.getStringExtra(Intent_AppVesion)
+        val versionName = intent.getStringExtra(intent.getStringExtra(Intent_AppVesion)) ?: ""
         val year = Calendar.getInstance().get(Calendar.YEAR)
-        txtCopyrights.text = String.format(getString(R.string.copyright1), versionName, year)
+        txtCopyrights.text = String.format(getString(R.string.copyright), versionName, year)
     }
 
-    private fun reportBug(){
-        val mIntent = Intent(Intent.ACTION_SENDTO)
-        mIntent.data = Uri.parse("mailto:")
-        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emailId))
-        mIntent.putExtra(Intent.EXTRA_SUBJECT, intent.getStringExtra(Intent_AppName) + " (v-"+ intent.getStringExtra(Intent_AppVesion) + " -- "+ getString(R.string.report_bug_desc))
-        startActivity(Intent.createChooser(mIntent, "Send Email"))
-    }
 
 }
