9ac88bd894cbcd14ba06b122d513a8e9c1cb220f
rajashakher.valluru <rajashakher.valluru@ril.com>
2019-07-11 15:52:24 +0530

    Same as previous game

---
+++ src/main/java/com/tinny/commons/helper/AppLoger.java
@@ -0,0 +1,27 @@
+package com.tinny.commons.helper;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+/**
+ * This class use to create the application logs
+ */
+
+public class AppLoger {
+    public static void debugLogs(String TAG, String message) {
+        Log.d(TAG, message);
+    }
+
+    public static void errorLogs(String TAG, String message) {
+        Log.e(TAG, message);
+
+    }
+
+
+}
